9dc5a4dc6873b4ed3186c58238a8c3e2d4c7e8c7
shomich <galiullintimur@gmail.com>
2017-10-11 01:01:36 +0300

    remove unused files

--- fluent-api/src/main/java/ru/sbt/i9n/o11n/fluent/ValidateState.java
+++
@@ -1,8 +0,0 @@
-package ru.sbt.i9n.o11n.fluent;
-
-/**
- * Created by SIGMA\sbt-galiullin-ts on 09.08.17.
- */
-public interface ValidateState extends State {
-
-}
--- fluent-api/src/main/java/ru/sbt/i9n/o11n/fluent/impl/state/Endpoint.java
+++
@@ -1,7 +0,0 @@
-package ru.sbt.i9n.o11n.fluent.impl.state;
-
-/**
- * Created by shomich on 26.09.17.
- */
-public interface Endpoint {
-}
